008f480ffbf803d5c8665eb92db2fe70fdca6469
Brandon Avant <avant.brandon@gmail.com>
2022-10-21 15:56:53 -0500

    Ignore Migrations sub-directory

--- src/ORMDemo.API/Infrastructure/Entities/Blog.cs
+++ src/ORMDemo.API/Infrastructure/Entities/Blog.cs
@@ -3,6 +3,7 @@ namespace ORMDemo.API.Infrastructure.Entities;
 public class Blog
 {
     public Guid BlogId { get; set; }
+    
     public DateTime CreatedTimestamp { get; set; }
     public DateTime ModifiedTimestamp { get; set; }
 
